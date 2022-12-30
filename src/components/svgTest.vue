8989de23dbab1e437bc8d7cc516717cd4665b411
Alex Test <alex@test.com>
2022-12-30 17:13:45 +0300

    Add svg icon

--- src/components/svgTest.vue
+++ src/components/svgTest.vue
@@ -1,5 +1,25 @@
 <template>
   <div class="svg">
+    <div class="a" style="color: blue">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 -24 24 24"
+      >
+        <g
+          fill-rule="evenodd"
+          fill="red"
+          stroke="blue"
+          color="black"
+          transform="rotate(270)"
+        >
+          <path d="M0 0h24v24H0z" />
+          <path stroke="#858585" d="M4 12.011l5 5L20.011 6" />
+        </g>
+      </svg>
+    </div>
+    <hr />
     <svg display="none">
       <symbol id="b" viewBox="0 0 524 524">
         <g>
