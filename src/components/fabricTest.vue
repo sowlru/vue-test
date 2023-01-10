26ea297602066c41427ca46779b537201e02aea9
Alex Test <alex@test.com>
2023-01-10 15:33:52 +0300

    Add JSON while loading in fabricTest

--- src/components/fabricTest.vue
+++ src/components/fabricTest.vue
@@ -20,13 +20,58 @@ export default {
   data() {
     return {
       //   canvas: null,
-      json: []
+      json: [],
+      json1: {
+        version: '5.3.0',
+        objects: [
+          {
+            type: 'rect',
+            version: '5.3.0',
+            originX: 'left',
+            originY: 'top',
+            left: 176.59,
+            top: 236.11,
+            width: 50,
+            height: 50,
+            fill: 'red',
+            stroke: null,
+            strokeWidth: 1,
+            strokeDashArray: null,
+            strokeLineCap: 'butt',
+            strokeDashOffset: 0,
+            strokeLineJoin: 'miter',
+            strokeUniform: false,
+            strokeMiterLimit: 4,
+            scaleX: 1,
+            scaleY: 1,
+            angle: 0,
+            flipX: false,
+            flipY: false,
+            opacity: 1,
+            shadow: null,
+            visible: true,
+            backgroundColor: '',
+            fillRule: 'nonzero',
+            paintFirst: 'fill',
+            globalCompositeOperation: 'source-over',
+            skewX: 0,
+            skewY: 0,
+            rx: 0,
+            ry: 0
+          }
+        ],
+        background: 'cyan'
+      }
     }
   },
   mounted() {
     canvas = new fabric.Canvas(this.$refs.canvas)
+    this.restoreJSON(this.json1)
     canvas.on({
       'mouse:dblclick': this.handleDblclick
+    })
+    canvas.on({
+      'after:render': this.handleAfterRender
     })
   },
   methods: {
@@ -57,8 +102,15 @@ export default {
       canvas.loadFromJSON(this.json, canvas.renderAll.bind(canvas))
       console.log('restore : canvas', canvas)
     },
+    restoreJSON(json) {
+      canvas.loadFromJSON(json, canvas.renderAll.bind(canvas))
+      console.log('restoreJSON : canvas', canvas)
+    },
     handleDblclick(e) {
       console.log('handleDblclick', e.target)
+    },
+    handleAfterRender(e) {
+      console.log('handleAfterRender', e.target)
     }
   }
 }
