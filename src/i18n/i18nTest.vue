e28d896b420e0133dc57519ca352145d5757831f
Alex Test <alex@test.com>
2023-01-31 21:36:08 +0300

    Add setLocale function in el-menu

--- src/i18n/i18nTest.vue
+++ src/i18n/i18nTest.vue
@@ -1,9 +1,23 @@
 <template>
   <div class="container">
-    <ol>
+    <!-- <ol>
       <li>npm install vue-i18n vue-flag-icon</li>
-    </ol>
+    </ol> -->
     <div>
+      <el-menu
+        :default-active="activeIndex"
+        class="el-menu-demo"
+        mode="horizontal"
+        @select="handleSelect"
+      >
+        <el-sub-menu index="2">
+          <template #title>Язык</template>
+          <el-menu-item @click="setLocale('ru')" index="2-1">RU</el-menu-item>
+          <el-menu-item @click="setLocale('en')" index="2-2">EN</el-menu-item>
+        </el-sub-menu>
+      </el-menu>
+      <el-switch v-model="isRu" active-text="Ru" inactive-text="En" />
+
       <button @click="setLocale('en')">en</button>
       <button @click="setLocale('ru')">ru</button>
     </div>
@@ -13,6 +27,16 @@
 <script>
 /* eslint-disable */
 export default {
+  data() {
+    return {
+      isRu: true
+    }
+  },
+  watch: {
+    isRu(newValue) {
+      this.$i18n.locale = newValue ? 'ru' : 'en'
+    }
+  },
   methods: {
     setLocale(locale) {
       this.$i18n.locale = locale
